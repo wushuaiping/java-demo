09c164e4d444c9acf95e9440bbff24b387a2b16c
Spring <spwu@creams.io>
2019-06-04 15:07:36 +0800

    add project gateway

---
+++ current-limiting-gateway/src/main/java/io/wooo/current/limiting/gateway/CurrentLimitingGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.wooo.current.limiting.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentLimitingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentLimitingGatewayApplication.class, args);
+	}
+
+}
